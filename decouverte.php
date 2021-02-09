140eb74152b6e1f538e1b09f57146c6c19e1481f
mathilderenversade <mathirenversade@gmail.com>
2021-02-09 11:25:16 +0100

    methode exec et requete preparée pour pdo

--- decouverte.php
+++ decouverte.php
@@ -44,8 +44,86 @@ $pdoStatement = $pdoDBConnexion->query($sql);
 var_dump($pdoStatement);
 
 // Etape 4, je récupère les résultats de la requête
-// fetchAll peut prendre différents paramètres qui me permettent de contrôler sous quelle forme me sont retournées les résultats, la documentation : https://www.php.net/manual/fr/pdostatement.fetch.php + https://www.php.net/manual/fr/pdostatement.fetchall.php
+// fetch et fetchAll peut prendre différents paramètres qui me permettent de contrôler sous quelle forme me sont retournées les résultats, la documentation : https://www.php.net/manual/fr/pdostatement.fetch.php + https://www.php.net/manual/fr/pdostatement.fetchall.php
+// fetchAll ça permet de récupérer tous les résultats de ma requête tandis que fetch me récupère un résultat à la fois.
 
 $resultats = $pdoStatement->fetchAll(PDO::FETCH_ASSOC);
 
-var_dump($resultats);
+//$resultats = $pdoStatement->fetch(PDO::FETCH_ASSOC);
+//var_dump($resultats);
+//$resultats2 = $pdoStatement->fetch(PDO::FETCH_ASSOC);
+//var_dump($resultats2);
+
+
+$sql = "
+    INSERT INTO `article` (`title`, `content`, `author`, `date`, `category`)
+    VALUES ('Le plus beau chien du monde', 'lorem ipsum', 'Mathilde', '2020-02-10', 'Loisirs');
+";
+
+$affectedRows = $pdoDBConnexion->exec($sql);
+var_dump($affectedRows);
+
+$title = 'Les jours sans fin de mon chien';
+$author = 'Grégory';
+$category = 'Sortie';
+$date = '2021-01-01';
+
+$sql = "
+    INSERT INTO `article` (`title`, `content`, `author`, `date`, `category`)
+    VALUES ('{$title}', 'lorem ipsum', '{$author}', '{$date}', '{$category}'); 
+";
+
+$newAffectedRows = $pdoDBConnexion->exec($sql);
+var_dump($newAffectedRows);
+
+
+
+$id = 15;
+
+$sql = "
+    DELETE FROM `article` WHERE id = '{$id}';
+";
+
+$affectedRows = $pdoDBConnexion->exec($sql);
+
+if ($affectedRows === false){
+    $response = "Problème dans la requête, erreur SQL";
+} else if ($affectedRows === 0){
+    $response = "Aucune modification a eu lieu dans la base de données, rien à changer";
+} else {
+    $response = "Requête a bien fonctionné, et ". $affectedRows ." lignes ont été modifiées en base de données"; 
+}
+
+echo $response;
+
+//var_dump($affectedRows);
+
+// Mettre en place une requête préparée est un moyen de sécuriser son code et de ne pas subir d'injection SQL, c'est-à-dire de manipulation frauduleuse des données stockées dans notre base de données via des inputs utilisateurs (exemple, via des inputs d'un formulaire). https://fr.wikipedia.org/wiki/Injection_SQL
+
+// Mise en place d'une requête préparée : 
+
+$title = 'Quel est le chien le plus intelligent ?';
+$author = 'Clément';
+
+$sql = "
+    INSERT INTO `article`(`title`, `content`, `author`, `date`, `category`)
+    VALUES (:title, 'lorem ipsum', :author, '2020-01-01', 'Ballades'); 
+";
+
+$requetePrepared = $pdoDBConnexion->prepare($sql);
+$requetePrepared->bindValue(':title', $title);
+$requetePrepared->bindValue(':author', $author);
+$result = $requetePrepared->execute();
+var_dump($result);
+
+// Je récapitule
+// Une fois que ma classe PDO a été instanciée et que donc j'ai un objet PDO (ici stocké dans la variable $pdoDBConnextion)
+// J'écris une requête SQL que je stocke dans une variable (ici $sql)
+// Si je veux lire des données de ma BDD, j'utilise la méthode query() sur mon objet PDO et je lui transmet ma requête SQL. Si je veux faire n'importe quelle autre action que lire, j'utilise la méthode exec().
+//J'exécute ma requête SQL et je récupère une boîte de résultats (exemple ma variable $pdoStatement)
+// pour ensuite récupérer mes résultats, j'utilise la méthode fetch ou fetchAll (en fonction du nombre de résultats que je souhaite récupérer) sur ma boîte de résultats :
+// exemple : ($resultats = $pdoStatement->fetchAll(PDO::FETCH_ASSOC);)
+// Via les paramètres de fetch et fetchAll je peux préciser le format dans lequel je veux ces résultats.
+// enfin je peux boucler sur mes résultats et les afficher par exemple ou bien faire un traitement sur ces résultats (= les données que j'ai récupérées en bdd)
+
+// enfin dans le cas où j'exécute des requêtes SQL dont des valeurs sont issues d'input utilisateurs, il faut faire attention aux scripts malveillants (NEVER TRUST THE USER INPUT) et donc faire une requête préparée.
